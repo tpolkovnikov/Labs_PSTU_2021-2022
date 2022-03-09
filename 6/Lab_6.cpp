45410220ede2aef0a0912b45a5b3a9748d1b46c5
tpolkovnikov <88273241+tpolkovnikov@users.noreply.github.com>
2022-03-09 22:28:58 +0300

    Add files via upload

---
+++ 6/Lab_6.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	char line[255];
+	cout << "¬ведите строку: " << endl;
+	gets_s(line, 255);
+	string result = "";
+	string word = "";
+	for (char c : line) {
+		if (c != ' ' && c != '.') {
+			word += c;
+		}
+		if (c == ' ' || c == '.') {
+			string temp = word;
+			reverse(temp.begin(), temp.end());
+			if (word == temp) {
+				result += word;
+				result += " ";
+			}
+			word = "";
+		}
+	}
+	cout << result;
+	return 0;
+}
