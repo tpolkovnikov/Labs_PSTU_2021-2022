1b153fb84496e8c419976c4d7082d4bbff7edf72
tpolkovnikov <88273241+tpolkovnikov@users.noreply.github.com>
2022-03-09 13:11:16 +0300

    Add files via upload

---
+++ 4/Lab_4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	setlocale(LC_ALL, "Rus");
+	int const size = 10;  // длинна массива
+	int arr[size];
+	// заполнение массива
+	srand(unsigned(time(0)));               
+	for (int i = 0; i < size; i++) {
+		arr[i] = rand() % 100 + 1;
+	}
+	// вывод массива
+	cout << "Ќачальный массив: ";
+	for (int i = 0; i < size; i++) {   
+		cout << arr[i] << ' ';
+	}
+	cout << std::endl;
+	// удаление первых 5-и элементов
+	for (int i = 0; i < size; i++) {  
+		arr[i] = arr[i + 5];
+	}
+	// добавление 3-х элементов
+	for (int i = size - 5; i < size - 5 + 3; i++) {  
+		arr[i] = rand() % 100 + 1;	
+	}
+	cout << " онечный массив: ";
+	// вывод массива
+	for (int i = 0; i < size - 5 + 3; i++) {  
+		cout << arr[i] << ' ';
+	}
+	cout << std::endl;
+	return 0;
+}
